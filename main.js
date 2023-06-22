f9c0aeba4936eef0102374547517953631cb4085
Luchioffice <uluann1@gmail.com>
2023-06-22 10:21:11 +0100

    nine commit

--- main.js
+++ main.js
@@ -28,36 +28,5 @@ picAnimate.forEach((el) => observer.observe(el));
 
 
 
-document.getElementById('myForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent form from submitting normally
-    
-    // Get form values
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var cname = document.getElementById('cname').value;
-    var message = document.getElementById('message').value;
-    
-    // Create the email body
-    var body = 'Name: ' + name + '\n' +
-               'Email: ' + email + '\n' +
-               'Company Name: ' + cname + '\n' +
-               'Message: ' + message;
-    
-    // Send email using EmailJS
-    emailjs.send("service_xnxprod", "template_zdjz90e", {
-      to_email: 'callmeoluchi@gmail.com',
-      from_name: 'Ann',
-      from_email: 'uluann1@gmail.com',
-      message: body
-    })
-    .then(function(response) {
-      console.log("SUCCESS", response.status, response.text);
-      alert('Email sent successfully!');
-      // Optionally, you can clear the form inputs after successful submission
-      document.getElementById('myForm').reset();
-    }, function(error) {
-      console.log("FAILED", error);
-      alert('Failed to send email. Please try again later.');
-    });
-  });
+
   
